efcfe0fd89a9348edc48b7a1c482b94191275d32
sirilXspace <siriluhd@gmail.com>
2025-10-16 12:41:51 +0530

    until offer

--- js/swiper-call.js
+++ js/swiper-call.js
@@ -108,8 +108,8 @@ var topbrandslider = new Swiper(".topbrand-slider", {
   slidesPerView: 3,
   spaceBetween: 5,
   navigation: {
-    nextEl: ".swiper-button-next1n",
-    prevEl: ".swiper-button-prev1n",
+    nextEl: ".swiper-button-next2n",
+    prevEl: ".swiper-button-prev2n",
   },
   breakpoints: {
     1340: {
@@ -131,6 +131,69 @@ var topbrandslider = new Swiper(".topbrand-slider", {
     600: {
       slidesPerView: 4,
       spaceBetween: 25,
+    },
+  },
+});
+
+
+var currenttopsellersslider = new Swiper(".currenttopsellers-slider", {
+  slidesPerView: 2,
+  spaceBetween: 10,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    // when window width is >= 1280
+    1280: {
+      slidesPerView: 6,
+      spaceBetween: 15,
+    },
+    // when window width is >= 1024
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 15,
+    },
+    // when window width is >= 992
+    992: {
+      slidesPerView: 4,
+      spaceBetween: 15,
+    },
+    // when window width is >= 575
+    575: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+  },
+});
+
+var offersmobileslider = new Swiper(".offersmobile-slider", {
+  slidesPerView: 2,
+  spaceBetween: 10,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    1280: {
+      slidesPerView: 4,
+      spaceBetween: 15,
+    },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 15,
+    },
+    767: {
+      slidesPerView: 2,
+      spaceBetween: 15,
+    },
+    600: {
+      slidesPerView: 1,
+      spaceBetween: 15,
+    },
+    575: {
+      slidesPerView: 1,
+      spaceBetween: 10,
     },
   },
 });
